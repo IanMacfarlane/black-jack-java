3b24fe7296d02f9778db6e774ce2ec8669a3b895
Ian Macfarlane <iandmacfarlane13@gmail.com>
2019-04-08 17:04:27 -0600

    Working perfectly

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,8 +1,76 @@
+import java.util.Scanner;
+
 public class BlackJack {
 
 	public static void main(String[] args) {
-		Deck deck = new Deck();
+		
+		Scanner console = new Scanner(System.in);
 
+		Deck deck = new Deck();
 		Hand playerHand = new Hand();
+		Hand dealerHand = new Hand();
+
+		playerHand.insert(deck.draw());
+		dealerHand.insert(deck.draw());
+		playerHand.insert(deck.draw());
+		dealerHand.insert(deck.draw());
+
+		String input = "none";
+		boolean playerBust = false;
+		boolean dealerBust = false;
+		do {
+			System.out.println("You have "+playerHand.toString()+"totaling "+playerHand.getTotal());
+			if (playerHand.getTotal() > 21) {
+				playerHand.hasAce();
+				if (playerHand.getTotal() > 21) {
+					playerBust = true;
+				}
+			}
+			else if (playerHand.getTotal() == 21) {
+				input = "stay";
+			}
+			else if (playerBust == false) {
+				do {
+				System.out.print("Would you like to (hit) or (stay): ");
+				input = console.nextLine();
+				} while (!input.equals("hit") && !input.equals("stay"));	
+
+				if (input.equals("hit")) {
+					playerHand.insert(deck.draw());
+				}
+			}
+
+
+		} while (!input.equals("stay") && !playerBust);
+
+		boolean win = true;
+
+		if (playerBust == true) {
+			win = false;
+		}
+		else if (playerHand.getTotal() == 21) {
+
+		}
+		else {
+			while (dealerHand.getTotal() < 17) {
+				dealerHand.insert(deck.draw());
+				if (dealerHand.getTotal() > 21) {
+					dealerHand.hasAce();
+				}
+			}
+			System.out.println("The dealer has "+dealerHand.toString()+"totaling "+dealerHand.getTotal());
+			if (dealerHand.getTotal() > 21) {
+				dealerBust = true;
+			}
+			else if (dealerHand.getTotal() > playerHand.getTotal()) {
+				win = false;
+			}
+		}
+		if (win) {
+			System.out.println("You win!");
+		}
+		else {
+			System.out.println("The dealer wins!");
+		}
 	}
 }
